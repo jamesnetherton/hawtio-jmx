563f0106221db800839e106af7a71759f0a6ece4
James Netherton <jamesnetherton@gmail.com>
2018-02-02 12:28:39 +0000

    wip

--- plugins/jvm/ts/jolokia.service.ts
+++ plugins/jvm/ts/jolokia.service.ts
@@ -19,10 +19,9 @@ namespace JVM {
       });
     }
 
-    executeOperation(mbean: string, operation: string, args: any[] = []): ng.IPromise<any> {
+    executeOperation(mbean: string, operation: string, ...args: any[]): ng.IPromise<any> {
       return this.$q((resolve, reject) => {
-        this.jolokia.request(
-          { type: 'exec', mbean: mbean, operation: operation, arguments: args },
+        this.jolokia.execute(mbean, operation, args,
           { success: response => resolve(response.value) },
           { error: response => {
               log.error(`JolokiaService.executeOperation('${mbean}', '${operation}', '${args}') failed. Error: ${response.error}`);
